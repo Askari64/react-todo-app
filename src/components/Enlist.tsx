31b6f9219833033ab668f99fdae4ff2a71da4fc3
Askari <askarir64@gmail.com>
2023-09-12 16:46:11 +0530

    input reset on adding to LS && sucessful loading on change of items

--- src/components/Enlist.tsx
+++ src/components/Enlist.tsx
@@ -1,31 +1,58 @@
-import { useState } from "react";
+import { useState , useEffect} from "react";
 
 function Enlist() {
   const [listing, setListing] = useState("");
+  const [items, setItems] = useState<{key: string; value: string | null}[]> ([]);
+  const [clicked, setClicked] = useState(false);
 
   const addToLocalStorage = () => {
+
+    if( listing == '') {
+      console.log('nothing entered')
+      return
+    }
     const randomKey = self.crypto.randomUUID();
     console.log(randomKey);
 
     localStorage.setItem(randomKey, listing);
     console.log(localStorage);
+    setClicked(!clicked)
+    setListing('');
   };
+ // useEffect hook to update the items state whenever localStorage changes
+ useEffect(() => {
+  // get all the keys from localStorage
+  const keys = Object.keys(localStorage);
+  // map each key to an object with key and value properties
+  const mappedItems = keys.map((key) => ({
+    key: key,
+    value: localStorage.getItem(key),
+  }));
+  // update the items state with the mapped items
+  setItems(mappedItems);
+}, [localStorage, clicked]);
 
-  return (
-    <>
-      <label>Task:</label>
-      <input
-        aria-label="Task"
-        type="text"
-        onChange={(e) => {
-          setListing(e.target.value); // Update input value in state
-        }}
-      />
-      <button type="button" onClick={addToLocalStorage}>
-        Add
-      </button>
-    </>
-  );
+return (
+  <>
+    <label>Task:</label>
+    <input
+      aria-label="Task"
+      type="text"
+      value={listing}
+      onChange={(e) => {
+        setListing(e.target.value); // Update input value in state
+      }}
+    />
+    <button type="button" onClick={addToLocalStorage}>
+      Add
+    </button>
+    <ul>
+      {items.map((item) => (
+        <li key={item.key}>{item.value}</li>
+      ))}
+    </ul>
+  </>
+);
 }
 
 export default Enlist;
